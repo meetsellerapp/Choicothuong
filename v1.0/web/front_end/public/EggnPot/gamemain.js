9220c5e83e13b7a77e22e822576f38f63ff4004e
tuancat <tuan.catnghiemhieu@gmail.com>
2015-11-24 22:11:42 +0700

    - update load game from firebase

--- v1.0/web/front_end/public/EggnPot/gamemain.js
+++ v1.0/web/front_end/public/EggnPot/gamemain.js
@@ -1,4 +1,22 @@
 jQuery(document).ready(function ()
 {
     console.log("aaaaa");
+    var currentGame = new Object();
+    function loadObjectGame(snapshot) {
+        currentGame.name = snapshot.val().name;
+        currentGame.prize_number = snapshot.val().prize_number;
+        currentGame.prize_str = snapshot.val().prize_str;
+        currentGame.source_url = snapshot.val().source_url;
+        currentGame.top_score = snapshot.val().top_score;
+        currentGame.top_score_id = snapshot.val().top_score_id;
+        currentGame.top_score_time = snapshot.val().top_score_time;
+    }
+    function getGameData() {
+        var ref = new Firebase("https://choicothuong.firebaseio.com/game");
+        ref.orderByChild("source_url").equalTo("/egg").on("child_added", function (snapshot) {
+            loadObjectGame(snapshot);
+            console.log(currentGame.top_score_time);
+        });
+    }
+    getGameData();
 });
