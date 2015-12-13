63e34bb58ae05e89824a9626ff8006dc6bdaeaa8
tuancat <tuan.catnghiemhieu@gmail.com>
2015-12-13 11:14:14 +0700

    - update game

--- v1.0/web/front_end/public/js/gamefunction.js
+++ v1.0/web/front_end/public/js/gamefunction.js
@@ -1,8 +1,19 @@
 function openDialog() {
-    
-    
+
+
     jQuery(document).ready(function () {
         console.log("open dialog");
         WEPAPP.Modal.fullViewDialog('#diaLogModal');
     });
+}
+function updateTimeScore(time , score) {
+    console.log("updateTime:" + time);
+    console.log("updateScore:" + score);
+}
+function updateFinalTimeScore(time , score) {
+    console.log("updateFinalTime:" + time);
+    console.log("updateFinalScore:" + score);
+}
+function startGame() {
+    console.log("startGame:----------------------------");
 }
